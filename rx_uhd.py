cc4cdf81b6779ab74c9fd9e0479952c7062a317d
Markus <makro_94@hotmail.com>
2020-04-05 17:36:38 +0200

    removed old version of rx code

--- rx_uhd.py
+++ rx_uhd.py
@@ -24,7 +24,22 @@ import re
 import os
 import iono_config
 
-#import cc_ram # analysis
+import os
+import psutil
+
+def tune_at(u,t0,f0=4e6):
+    """ 
+    tune radio to frequency f0 at t0_full 
+    use a timed command.
+    """
+    u.clear_command_time()
+    t0_ts=uhd.libpyuhd.types.time_spec(t0)
+    print("tuning at %1.2f"%(t0_ts.get_real_secs()))
+    u.set_command_time(t0_ts)
+    tune_req=uhd.libpyuhd.types.tune_request(f0)
+    u.set_tx_freq(tune_req)
+    u.set_rx_freq(tune_req)
+    u.clear_command_time()
 
 def delete_old_files(t0,data_path="/dev/shm"):
     """
@@ -43,50 +58,137 @@ def delete_old_files(t0,data_path="/dev/shm"):
             
 
 def write_to_file(recv_buffer,fname,log,dec=10):
-        
+    print("writing to file %s"%(fname))
     obuf=stuffr.decimate(recv_buffer,dec=dec)
     obuf.tofile(fname)
 
-def receive_waveform(u,t0_full,f0,recv_buffer,log=None,N=10000000,file_idx=0,sweep_idx=0):
-    t0_ts=uhd.libpyuhd.types.time_spec(np.uint64(t0_full),0.0)
-        
-    # wait for moment right before transmit
-    t_now = u.get_time_now().get_real_secs()
-    t_left = t0_full - t_now
-    print(t_now)
-    print(t_left)
-    
-    if (t_left-0.1) > 0.0:
-        # sleep until we're ready to go
-        time.sleep(t_left-0.1)
+def receive_continuous(u,t0,t_now,s,log,sample_rate=1000000.0):
+    """
+    New receive script, which processes data incoming from the usrp
+    one packet at a time.
+    """
 
-    print("Tuning to %1.2f"%(f0))
-    tune_req=uhd.libpyuhd.types.tune_request(f0)
-    u.set_rx_freq(tune_req)
-    
-    print("Launching receive at %1.2f now=%1.2f"%(t0_full,t_now))
+    # sweep timing and frequencies
+    fvec=[]
+    t0s=[]    
+    for i in range(s.n_freqs):
+        f,t=s.pars(i)
+        fvec.append(f)
+        t0s.append(t)
 
-    stream_args=uhd.usrp.libtypes.StreamArgs("fc32","sc16")    
+    # setup usrp to stream continuously, starting at t0
+    stream_args=uhd.usrp.StreamArgs("fc32","sc16")    
     rx_stream=u.get_rx_stream(stream_args)
-    stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.num_done)
-    stream_cmd.num_samps=N
+    stream_cmd = uhd.types.StreamCMD(uhd.types.StreamMode.start_cont)
     stream_cmd.stream_now=False
-    stream_cmd.time_spec=uhd.types.TimeSpec(t0_full)
+    stream_cmd.time_spec=uhd.types.TimeSpec(t0)
     rx_stream.issue_stream_cmd(stream_cmd)
     md=uhd.types.RXMetadata()
-    # this part could be improved by processing the stream one
-    # packet at a time and padding zeros when a drop is
-    # detected.
-    num_rx_samps=rx_stream.recv(recv_buffer,md,timeout=20.0)
-    if num_rx_samps != N:
-        if log != None:
-            log.log("Dropped packet %d read."%(num_rx_samps))
-            
-    write_thread=threading.Thread(target=write_to_file,args=(recv_buffer,"%s/raw-%d-%03d.bin"%(iono_config.data_path,sweep_idx,file_idx),log))
-    write_thread.start()
+
+    # this is how many samples we expect to get from each packet
+    max_samps_per_packet = rx_stream.get_max_num_samps()
+
+    # receive buffer size large enough to fit one packet
+    recv_buffer=n.zeros(max_samps_per_packet,dtype=n.complex64)
+
+    # initial timeout is long enough for us to receive the first packet, which
+    # happens at t0
+    timeout=(t0-t_now)+5.0
+
+    # store data in this ringbuffer, and offload it to ram disk once
+    # one full cycle is finished
+    output_buffer = n.zeros(2*int(s.freq_dur*sample_rate),dtype=n.complex64)
+
+    # use this buffer to write to a file
+    wr_buff = n.zeros(int(s.freq_dur*sample_rate),dtype=n.complex64)
+    bl=len(output_buffer)
+    bi=0
+
+    # frequency index
+    fi = 0
+    # samples since 1970 for the previous packet (no previous packet at first)
+    prev_samples = -1
+
+    # samples since 1970 for the first packet. 
+    samples0=int(stream_cmd.time_spec.get_full_secs())*int(sample_rate) + int(stream_cmd.time_spec.get_frac_secs()*sample_rate)
     
-    print("done %1.2f timespec %1.2f num_samps %d"%(time.time(),md.time_spec.get_real_secs(),num_rx_samps))
-    return(write_thread)
+    # number of samples per frequency in the sweep
+    n_per_freq=int(s.freq_dur*sample_rate)
+    n_per_sweep=int(s.sweep_len_s*sample_rate)    
+
+    sweep_num=0
+    freq_num=0    
+    next_sample = samples0 + n_per_freq
+    cycle_t0 = t0
+
+    tune_at(u,t0+s.freq_dur,f0=s.freq(1))
+    
+    while True:
+        num_rx_samps=rx_stream.recv(recv_buffer,md,timeout=timeout)
+        if num_rx_samps == 0:
+            # shit happened. we probably lost a packet. gotta try again
+            log.log("dropped packet. number of received samples is 0")
+            continue
+        
+        # the start of the buffer is at this sample index
+        samples=int(md.time_spec.get_full_secs())*int(sample_rate) + int(md.time_spec.get_frac_secs()*sample_rate)
+
+        # this is how many samples we have jumped forward.
+        step = samples-prev_samples
+
+        if prev_samples == -1:
+            step = 363
+            
+        if step != 363 or num_rx_samps != 363:
+            log.log("anomalous step %d num_rx_samps %d "%(step,num_rx_samps))
+            
+        prev_samples=samples
+
+        # write the result into the output buffer
+        output_buffer[ n.mod(bi+n.arange(num_rx_samps,dtype=n.uint64),bl) ]=recv_buffer
+
+        bi=bi+step
+        
+        if samples > next_sample:
+            # this should be correct now.
+            idx0=sweep_num*n_per_sweep+freq_num*n_per_freq
+            
+            wr_buff[:]=output_buffer[n.mod(idx0+n.arange(n_per_freq,dtype=n.uint64),bl)]
+            
+            wr_thread=threading.Thread(target=write_to_file,args=(wr_buff,"%s/raw-%d-%03d.bin"%(iono_config.data_path,cycle_t0,freq_num),log))
+            wr_thread.start()
+            freq_num += 1
+
+            # setup tuning for next frequency
+            tune_at(u,cycle_t0 + (freq_num+1)*s.freq_dur,f0=s.freq(freq_num+1))
+            print("Tuning to %1.2f at %1.2f"%(s.freq(freq_num+1)/1e6,cycle_t0 + (freq_num+1)*s.freq_dur))
+            
+            # we cycle over
+            if freq_num == s.n_freqs:
+                cycle_t0 += s.sweep_len_s
+                freq_num=0
+                sweep_num+=1
+                log.log("Starting new cycle")
+                
+                
+            
+            # we've got a full freq step
+            next_sample += n_per_freq
+            
+
+        # timestamp of first sample
+      #  t_head=md.time_spec.get_real_secs()
+
+      #  t_sweep=n.mod(t_head-t0,s.sweep_len_s)
+     #   n_sweep = int(n.floor(t_sweep/s.freq_dur))
+    #    if n_sweep != fi:
+   #         print("freq %d tuning to %1.2f MHz at %1.6f"%(n_sweep,fvec[n_sweep]/1e6,t_head))
+  #          tune_req=uhd.libpyuhd.types.tune_request(fvec[n_sweep])
+ #           u.set_rx_freq(tune_req)
+#            fi=n_sweep
+        
+        timeout=0.1
+
 
     
 def main():
@@ -94,8 +196,10 @@ def main():
     Start up everything and run main loop from here.
     """
     # setup a logger
-    log=l.logger("rx-%d.log"%(time.time()))
+    logfile="rx-%d.log"%(time.time())
+    log=l.logger(logfile)
     log.log("Starting receiver")
+    os.system("rm rx-current.log;ln -s %s rx-current.log"%(logfile))
     
     s=iono_config.s
     log.log("Sweep freqs:")
@@ -114,41 +218,33 @@ def main():
     subdev_spec=uhd.usrp.SubdevSpec(iono_config.rx_subdev)
     usrp.set_rx_subdev_spec(subdev_spec)
 
-    print("tuning")
-    tune_req=uhd.libpyuhd.types.tune_request(1e6)
-    usrp.set_rx_freq(tune_req)
-    print("tuning done")
-    
     # Synchronizing clock
     gl.sync_clock(usrp,log)
 
-    t_now = usrp.get_time_now().get_real_secs()
-    # figure out when to start the cycle. 
+    # figure out when to start the cycle.
+    t_now=usrp.get_time_now().get_real_secs()
     t0=np.uint64(np.floor(t_now/(s.sweep_len_s))*s.sweep_len_s+s.sweep_len_s)
-    
-    print("starting next sweep at %1.2f"%(t0))
-    
-    # double buffer
-    recv_buffers = [np.empty(N,dtype=n.complex64),np.empty(N,dtype=n.complex64)]
-    buf_idx=0
+    print("starting next sweep at %1.2f in %1.2f s, time now %1.2f"%(t0,t0-t_now,t_now))
 
-    print("Starting loop")
+    # start with initial frequency
+    tune_req=uhd.libpyuhd.types.tune_request(s.freq(0))
+    usrp.set_rx_freq(tune_req)
+    
+    
+    # start reading data
+    read_thread=threading.Thread(target=receive_continuous,args=(usrp,t0,t_now,s,log))
+    read_thread.start()
+
     while True:
-        log.log("Del thread")
-        del_thread=threading.Thread(target=delete_old_files,args=(int(t0)-int(s.sweep_len_s)*2,iono_config.data_path))
-        del_thread.start()
-        
-        log.log("Starting sweep")
-        
-        # run the sweep program
-        for i in range(s.n_freqs):
-            f0,dt=s.pars(i)
-            receive_waveform(usrp,np.uint64(t0+dt),f0,recv_buffer=recv_buffers[buf_idx],log=log,N=N-100000,file_idx=i,sweep_idx=t0)
-            buf_idx=(buf_idx+1)%2
-        
+        t0=usrp.get_time_now().get_real_secs()
+        delete_old_files(int(t0)-int(s.sweep_len_s)*3,iono_config.data_path)
         gl.check_lock(usrp,log,exit_if_not_locked=True)
         t0+=np.uint64(s.sweep_len_s)
-        del_thread.join()
+
+        process = psutil.Process(os.getpid())
+        log.log("Memory use %1.5f (MB)"%(process.memory_info().rss/1e6))
+
+        time.sleep(s.sweep_len_s)
 
 
         
--- start_rx.sh
+++ start_rx.sh
@@ -5,7 +5,7 @@ sudo sysctl -w net.core.rmem_max=500000000
 sudo sysctl -w net.core.wmem_max=2500000
 while true;
 do
-    python3 rx_uhd_test.py
+    python3 rx_uhd.py
     sleep 10
 done
     
