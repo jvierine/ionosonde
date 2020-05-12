d0c9fc5bc2e6561d975090dd4ad4617b20ecca75
Markus <makro_94@hotmail.com>
2020-05-12 18:03:58 +0200

    moved waveform generator and plotter to main level

--- create_waveform.py
+++ create_waveform.py
@@ -20,8 +20,8 @@ doi:10.5194/amt-9-829-2016, 2016.
 import math
 from argparse import ArgumentParser
 
-import numpy
-import numpy as np
+import numpy as n
+
 import scipy.signal
 
 
@@ -29,23 +29,68 @@ import scipy.signal
 # having to store all actual codes. the seed can then
 # act as a sort of station_id.
 def create_pseudo_random_code(clen=10000, seed=0):
-    numpy.random.seed(seed)
-    phases = numpy.array(
-        numpy.sign(numpy.random.randn(clen)),
-        dtype=numpy.complex64,
-    )
+    n.random.seed(seed)
+    phases = n.array(n.exp(1j*n.random.rand(clen)*2*n.pi),dtype=n.complex64)
     return(phases)
 
 
 # oversample a phase code by a factor of rep
 def rep_seq(x, rep=10):
     L = len(x) * rep
-    res = numpy.zeros(L, dtype=x.dtype)
-    idx = numpy.arange(len(x)) * rep
-    for i in numpy.arange(rep):
+    res = n.zeros(L, dtype=x.dtype)
+    idx = n.arange(len(x)) * rep
+    for i in n.arange(rep):
         res[idx + i] = x
     return(res)
 
+def filter_waveform(waveform,
+                    sr=1e6,
+                    bandwidth=100e3):
+    """
+    Filter the waveform in such a way that it meets a 1% out of 
+    band power requirement. filter the code in such a way that there
+    is a delay of 200 microseconds at DC.  Assumes that waveform is periodic.
+    
+    The 200 microsecond delay is to ensure that the transmit pulse is 
+    well above the 0 range gate in a monostatic case, so that we 
+    can keep an eye on the direct transmitted signal range as 
+    part of the procedure of making sure that the transmitter and
+    receiver are in sync
+    """
+    # filter window
+    w = n.zeros(len(waveform), dtype=n.complex64)
+
+    # first try
+    fl = int(sr/bandwidth/2)*2
+    power_outside_band=1.0
+
+    fvec=n.fft.fftshift(n.fft.fftfreq(len(waveform),d=1.0/sr))
+    # which frequency bins are in the band
+    fidx=n.where(n.abs(fvec) < bandwidth/2.0)[0]
+    
+    waveform_f=n.fft.fft(waveform)
+    print("Searching for filter length")
+    while power_outside_band > 0.01:
+        
+        w[0:fl] = scipy.signal.flattop(fl)
+        # filter
+        aa = n.fft.ifft(n.fft.fft(w) * waveform_f)
+        # scale maximum amplitude to unity
+        a = aa / n.max(n.abs(aa))
+        a = n.array(a, dtype=n.complex64)
+        # remove filter time shift 
+        a=n.roll(a,-int(fl/2)+20)
+        # power spectrum
+        S=n.fft.fftshift(n.abs(n.fft.fft(a))**2.0)
+        P_tot=n.sum(S)
+        P_in=n.sum(S[fidx])
+        power_outside_band = 1.0-P_in/P_tot
+        print("fl %d power outside band %1.3f"%(fl,power_outside_band))
+        fl+=2
+    print("Using filter length of %d samples"%(fl-2))
+    return(a)
+    
+    
 
 #
 # lets use 0.1 s code cycle and coherence assumption
@@ -57,26 +102,18 @@ def waveform_to_file(station=0,
                      clen=10000,
                      oversample=10,
                      filter_output=False,
-                     filter_factor=1.0):
+                     sr=1e6,
+                     bandwidth=100e3):
     
-    ofname='code-l%d-b%d-%06d.bin' % (clen, oversample, station)
+    ofname='code-l%d-b%d-%06df_%dk.bin' % (clen, oversample, station,int(bandwidth/1e3))
     
     a = rep_seq(create_pseudo_random_code(clen=clen, seed=station),
                 rep=oversample)
     
     if filter_output:
-        w = numpy.zeros([oversample * clen], dtype=numpy.complex64)
-        fl = (int(2*oversample*1.9*filter_factor)) # 100 kHz < 1% outside band
-        w[0:fl] = scipy.signal.flattop(fl)
-        print("Filter length %d samples"%(fl))
-        # todo roll by fl
-               
-        aa = numpy.fft.ifft(numpy.fft.fft(w) * numpy.fft.fft(a))
-        a = aa / numpy.max(numpy.abs(aa))
-        a = numpy.array(a, dtype=numpy.complex64)
-        # remove filter time shift 
-        a=np.roll(a,-int(fl/2)+20)
-        a.tofile(ofname)
+        a=filter_waveform(a,
+                          sr=sr,
+                          bandwidth=bandwidth)
         
     print("Writing file %s"%(ofname))
     a.tofile(ofname)
@@ -85,17 +122,17 @@ def waveform_to_file(station=0,
 def barker_to_file(
     station=0, clen=10000, oversample=10, filter_output=False,
 ):
-    a=np.zeros(clen*oversample,dtype=np.complex64)
-    barker13=np.array([1,1,1,1,1,-1,-1,1,1,-1,1,-1,1],dtype=np.complex64)
+    a=n.zeros(clen*oversample,dtype=n.complex64)
+    barker13=n.array([1,1,1,1,1,-1,-1,1,1,-1,1,-1,1],dtype=n.complex64)
     barker130=rep_seq(barker13,rep=oversample)
     a[0:130]=barker130
     print(len(a))
-    w = numpy.zeros([oversample * clen], dtype=numpy.complex64)
+    w = n.zeros([oversample * clen], dtype=n.complex64)
     fl = (int(2*oversample))
     w[0:fl] = scipy.signal.blackmanharris(fl)
-    aa = numpy.fft.ifft(numpy.fft.fft(w) * numpy.fft.fft(a))
-    a = aa / numpy.max(numpy.abs(aa))
-    a = numpy.array(a, dtype=numpy.complex64)
+    aa = n.fft.ifft(n.fft.fft(w) * n.fft.fft(a))
+    a = aa / n.max(n.abs(aa))
+    a = n.array(a, dtype=n.complex64)
     a.tofile('code-barkerf.bin')
 
 
@@ -121,8 +158,13 @@ if __name__ == '__main__':
                 (default: %(default)s)''',
     )
     parser.add_argument(
-        '-w', '--filter_factor', type=float, default=1.0,
-        help='''Filter length factor (1 = 100 kHz, 2=50 kHz, 3.333=30 kHz
+        '-w', '--bandwidth', type=float, default=100.0,
+        help='''Code bandwidth in kHz
+        (default: %(default)s)''',
+    )
+    parser.add_argument(
+        '-r', '--sample_rate', type=float, default=1000000,
+        help='''Sample rate in Hz
         (default: %(default)s)''',
     )
 
@@ -130,5 +172,5 @@ if __name__ == '__main__':
 
     waveform_to_file(
         station=op.station, clen=op.length, oversample=op.oversampling,
-        filter_output=op.filter, filter_factor=op.filter_factor
+        filter_output=op.filter, bandwidth=1e3*op.bandwidth, sr=op.sample_rate
     )
