cfb4f76017b3b611234a644452c0503c3386aa52
Peje Nilsson <peje@irf.se>
2021-02-17 11:12:48 +0100

    Improve logger
    Take a prefix instead of a name
    Move logs to subdir log
    Reopen log if new day
    Human readable date/time

--- iono_logger.py
+++ iono_logger.py
@@ -1,10 +1,35 @@
-import time
+#! /usr/bin/env python3
+
+import os
+from datetime import datetime, timedelta
+from pathlib import Path
+
 
 class logger:
-    def __init__(self,fname):
-        self.f=open(fname,"w")
-    def log(self,msg,print_msg=True):
+    def __init__(self, prefix):
+        self.logdir = Path("log")
+        self.logdir.mkdir(exist_ok=True)
+
+        self.creation_time = datetime.utcnow()
+        self.prefix = prefix
+        fname = self.logdir / ("%s%s" % (self.prefix, self.creation_time.strftime("%FT%T.log")))
+        self.f = open(fname, "w")
+
+        current_log = Path("%scurrent.log" % prefix)
+        if current_log.exists():
+            current_log.unlink()
+        current_log.symlink_to(fname)
+
+    def need_to_reopen(self):
+        if self.creation_time.date() != datetime.utcnow().date():
+            # New day, need to reopen logfile
+            self.f.close()
+            self.__init__(self, self.prefix)
+
+    def log(self, msg, print_msg=True):
+        self.need_to_reopen()
         if print_msg:
             print(msg)
-        self.f.write("%d %s\n"%(time.time(),msg))
+        log_time = datetime.utcnow()
+        self.f.write("%s %s\n" % (log_time.strftime("%FT%T"), msg))
         self.f.flush()
