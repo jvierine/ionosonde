d4f377d39c76facdb44ef870bdbd52c0e72fd6a6
Peje Nilsson <peje@irf.se>
2021-02-18 08:06:08 +0100

    Wait a complete sweep before analysing

--- start_an.sh
+++ start_an.sh
@@ -5,10 +5,28 @@ source config.sh
 # remove cache
 rm -f waveforms/cache*.h5
 
+# Get values from current iono_config
+. <(grep "^frequency_duration" $IONO_CONFIG | tr -d [:space:])
+NFREQS=$(python3 iono_config.py $IONO_CONFIG |grep -c "^t=")
+
+# Length of a sweep over all frequencies
+SWEEP_LEN=$((frequency_duration * NFREQS))
+# add 5 sec for finnish saving files
+EXTRA=5
+
+# remove cache
+rm -f waveforms/cache*.h5
+
 while true;
 do
     python3 analyze_ionograms.py $IONO_CONFIG
-    sleep 10
-#    python3 overview_plots.py $IONO_CONFIG
-done
+#     python3 overview_plots.py $IONO_CONFIG
 
+    # current time in seconds
+    S=$(date -u +%s)
+
+    # calculate number of seconds to wait until next sweep
+    WAIT=$(((SWEEP_LEN+EXTRA) - (S % SWEEP_LEN)))
+    echo "Waiting $WAIT seconds"
+    sleep $WAIT
+done
