189af34ae8652e0e8f5238b0caaba874249e8715
Peje Nilsson <peje66@gmail.com>
2021-04-29 14:08:20 +0200

    Tell we are waiting

--- start_rx.sh
+++ start_rx.sh
@@ -2,9 +2,11 @@
 
 source config.sh
 
+WAIT=10
 while true;
 do
     python3 rx_uhd.py --config=$IONO_CONFIG
-    sleep 10
+    echo "Waiting $WAIT seconds"
+    sleep $WAIT
 done
 
--- start_tx.sh
+++ start_tx.sh
@@ -2,8 +2,10 @@
 
 source config.sh
 
+WAIT=10
 while true;
 do
     python3 tx_uhd.py --config=$IONO_CONFIG
-    sleep 10
+    echo "Waiting $WAIT seconds"
+    sleep $WAIT
 done
